4cf3ae2a8ea51b1035c5793cd100a3813f6e496b
Foodzz <96932153+iLiekFoodz@users.noreply.github.com>
2024-02-22 15:51:57 -0500

    Add files via upload

---
+++ Lab_Four_ScriptOne_Nouh.m
@@ -0,0 +1,39 @@
+    %Nouh Shaikh
+%Lab 4 Homework Script 1
+
+clear,clc
+% A pipeline at an oil refinery is carrying oil to a large storage tank. The pipe
+% has a 20 inch internal diameter. The oil is flowing at 5 ft/s and its density is
+% 57 lb/ft3. What is the mass flow rate of oil in SI units? What is the mass and
+% volume of oil, in SI units, that flows in a 24 hour period? The flow rate of oil
+% is given by: 𝑚̇ = pvA
+
+%Question 1 for Lab 4
+
+%Flow Rate [ft/s -> m/s]
+FlowRate = input('Enter the Flow Rate in (ft/s):')
+FA1 = FlowRate/3.2808399;
+
+%Liquid Density [lb/ft3 -> kg/m3]
+LiquidDensity = input('Enter the Liquid Density in (lb/ft^3):')
+LDA1 = LiquidDensity*16.0184634;
+
+%Internal Diameter [lb/ft3 -> kg/m3]
+InternalDiameter = input('Enter the Internal Diamemeter in (inches):')
+IDA1 = InternalDiameter/39.3700787;
+
+%Converting meters to radius
+Radius = IDA1/2;
+%Finding the Area
+Area = pi.*(Radius).^2;
+
+%Providing the solution Mass Flow Rate of Oil
+
+MFRO = LDA1.*FA1.*Area;
+
+disp('The flow rate is:')
+disp(MFRO)
+
+
+
+
---
+++ Lab_Four_ScriptTwo_Nouh.m
@@ -0,0 +1,42 @@
+%Nouh Shaikh
+%Lab 4 Homework Script 2
+
+%Question 2 for Lab 4
+
+clear,clc
+%Notes below the input code indicate the designated value to be inputed for
+%the question given in LAB.
+
+%User inputs the value for Voltage.
+V = input('Input the Voltage (V):')
+% V = 230
+
+%User inputs the value for Hertz.
+f = input('Input the Hertz (Hz):')
+%f = 50 
+
+%User inputs the value for Omega.
+R = input('Input the Omega Value:')
+%R = 500 
+
+%User inputs the value for Inductance. 
+L = input('Input the Inductance Value (mH):')
+%L = 650
+
+%Calculation for W to rad/s
+w = 2*pi*(f)
+
+%Calculations for O
+o = atan(w*L/R)
+
+%Calculations for Z
+Z = R+i*w*L
+
+%Setting the t value for graph.
+t = 1:1:10;
+
+%Primary equation for calculating the inductance.
+tango = V/abs(Z)*((cos((w.*t)-o))-((exp(-t.*R/L))*cos(o)))
+
+plot(t, tango, '--r'), ylabel('Y-Axis'), xlabel('X-Axis'), grid
+
