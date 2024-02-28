aa91a7ac1665f107f853e9ea9d1a4434a5825124
Foodzz <96932153+iLiekFoodz@users.noreply.github.com>
2024-02-27 23:38:18 -0500

    Add files via upload

--- Celsuis_Fahrenheit.m
+++ Celsuis_Fahrenheit.m
@@ -1,12 +1,12 @@
 function [temp_table] = Celsuis_Fahrenheit (T_Lower,T_Upper)
-
+%Funtion command that defines low and high temps. 
 Celsuis = (T_Lower:1:T_Upper)';
 
 Fahrenheit = 9/5.*Celsuis+32;
-
+%Fahrenheit conversion from Celsuis
 temp_table=[Celsuis, Fahrenheit];
-
+%display the temperature once the command below is ran.
 end
 
-
+%the command to be run in the command window
 %[TABLE] =  Celsuis_Fahrenheit(0,25)
--- Fahrenheit_Celsuis.m
+++ Fahrenheit_Celsuis.m
@@ -1,11 +1,12 @@
 function [temp_table] =  Fahrenheit_Celsuis (T_Lower,T_Upper)
-
+%Funtion command that defines low and high temps. 
 Fahrenheit = (T_Lower:1:T_Upper)';
 
 Celsius = 5/9*(Fahrenheit-32);
-
+%Celsuis conversion from Fahrenheit
 temp_table = [Fahrenheit, Celsius];
+%display the temperature once the command below is ran.
 end
 
-
+%the command to be run in the command window
 %[TABLE] =  Fahrenheit_Celsuis(0,25)
