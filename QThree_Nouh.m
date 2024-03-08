f913626bbf815b81caad9e8f4e7b1430a8d8aee3
Foodzz <96932153+iLiekFoodz@users.noreply.github.com>
2024-03-07 23:59:51 -0500

    Add files via upload

---
+++ QThree_Nouh.m
@@ -0,0 +1,19 @@
+function variable = QThree_Nouh(x,y)
+
+    if x>=0
+        if y>=0
+    variable = (x+y);
+
+     else
+    variable = (x(y^2));
+
+    end 
+else
+   
+    if y>=0
+        variable = (x^2)+y;
+    else 
+        variable = (x^2)+(y^2);
+    end
+end
+
