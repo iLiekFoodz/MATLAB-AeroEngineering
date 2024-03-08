53784fb3d8d66648074e5d89bf9af2d33afd777a
Foodzz <96932153+iLiekFoodz@users.noreply.github.com>
2024-03-08 12:08:47 -0500

    Update QThree_Nouh.m

--- QThree_Nouh.m
+++ QThree_Nouh.m
@@ -1,19 +1,20 @@
 function variable = QThree_Nouh(x,y)
-
-    if x>=0
-        if y>=0
-    variable = (x+y);
+%We are evaluating a function f(x,y) where the user inputs x and y values
+%if x is larger or smaller than 0 it will use a certain function. 
+    if x>=0 %evaluating whether the input is greater than or equal to 0
+        if y>=0 %evaluating whether the input is greater than or equal to 0
+    variable = (x+y); %calculating it 
 
      else
-    variable = (x(y^2));
+    variable = (x(y^2)); %Calculating it again but checking if the value is less or large.
 
     end 
 else
    
-    if y>=0
+    if (y>=0 & x<=0) %evaluating whether the input is greater than or equal to 0 
         variable = (x^2)+y;
     else 
-        variable = (x^2)+(y^2);
+        variable = (x^2)+(y^2); %calculating 
     end
 end
 
